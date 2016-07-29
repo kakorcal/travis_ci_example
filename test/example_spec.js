cfbfd913571d8d59a8d75957809ea10b7ac279c0
Kenneth Korcal <kakorcal@gmail.com>
2016-07-29 14:09:49 -0700

    adding tests

---
+++ test/example_spec.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe('Adding', ()=>{
+  it('Should equal one', ()=>{
+    expect(1 + 0).to.equal(1);
+  });
+
+  it('Should equal two', ()=>{
+    expect(1 + 1).to.equal(2);
+  });
+});
